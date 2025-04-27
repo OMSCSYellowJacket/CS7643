211f8cbb0c93ad5a1b471e838af7abd569a69e24
Christian D. Powell <christian.david.powell@gmail.com>
2025-04-27 14:19:43 -0400

    Adds LSTM model for testing original paper data.

--- models/LSTM.py
+++ models/LSTM.py
@@ -1,124 +1,99 @@
-"""
-LSTM model.  (c) 2021 Georgia Tech
-
-Copyright 2021, Georgia Institute of Technology (Georgia Tech)
-Atlanta, Georgia 30332
-All Rights Reserved
-
-Template code for CS 7643 Deep Learning
-
-Georgia Tech asserts copyright ownership of this template and all derivative
-works, including solutions to the projects assigned in this course. Students
-and other users of this template code are advised not to share it with others
-or to make it available on publicly viewable websites including repositories
-such as Github, Bitbucket, and Gitlab.  This copyright statement should
-not be removed or edited.
-
-Sharing solutions with current or future students of CS 7643 Deep Learning is
-prohibited and subject to being investigated as a GT honor code violation.
-
------do not edit anything above this line---
-"""
-
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
 
 
 class LSTM(nn.Module):
-    # You will need to complete the class init function, and forward function
-
-    def __init__(self, input_size, hidden_size):
-        """ Init function for LSTM class
-            Args:
-                input_size (int): the number of features in the inputs.
-                hidden_size (int): the size of the hidden layer
-            Returns: 
-                None
-        """
+    def __init__(self, input_size=21, hidden_size=5, num_layers=4, output_size=1, dropout=0.2):
         super(LSTM, self).__init__()
-
         self.input_size = input_size
         self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.output_size = output_size
+        self.dropout = dropout
 
-        ################################################################################
-        # TODO:                                                                        #
-        #   Declare LSTM weights and attributes in order specified below to pass GS.   #
-        #   You should include weights and biases regarding using nn.Parameter:        #
-        #       1) i_t: input gate                                                     #
-        #       2) f_t: forget gate                                                    #
-        #       3) g_t: cell gate, or the tilded cell state                            #
-        #       4) o_t: output gate                                                    #
-        #   for each equation above, initialize the weights,biases for input prior     #
-        #   to weights, biases for hidden.                                             #
-        #   when initializing the weights consider that in forward method you          #
-        #   should NOT transpose the weights.                                          #
-        #   You also need to include correct activation functions                      #
-        ################################################################################
-        # i_t: input gate
-        self.W_ii = nn.Parameter(torch.randn(input_size, hidden_size))
-        self.b_ii = nn.Parameter(torch.zeros(hidden_size))
-        self.W_hi = nn.Parameter(torch.randn(hidden_size, hidden_size))
-        self.b_hi = nn.Parameter(torch.zeros(hidden_size))
-        self.sigmoid_it = nn.Sigmoid()
-        # f_t: the forget gate
-        self.W_if = nn.Parameter(torch.randn(input_size, hidden_size))
-        self.b_if = nn.Parameter(torch.zeros(hidden_size))
-        self.W_hf = nn.Parameter(torch.randn(hidden_size, hidden_size))
-        self.b_hf = nn.Parameter(torch.zeros(hidden_size))
-        self.sigmoid_ft = nn.Sigmoid()
-        # g_t: the cell gate
-        self.W_ig = nn.Parameter(torch.randn(input_size, hidden_size))
-        self.b_ig = nn.Parameter(torch.zeros(hidden_size))
-        self.W_hg = nn.Parameter(torch.randn(hidden_size, hidden_size))
-        self.b_hg = nn.Parameter(torch.zeros(hidden_size))
-        self.tanh_gt = nn.Tanh()
-        # o_t: the output gate
-        self.W_io = nn.Parameter(torch.randn(input_size, hidden_size))
-        self.b_io = nn.Parameter(torch.zeros(hidden_size))
-        self.W_ho = nn.Parameter(torch.randn(hidden_size, hidden_size))
-        self.b_ho = nn.Parameter(torch.zeros(hidden_size))
-        self.sigmoid_ot = nn.Tanh()
-        self.tanh_ot = nn.Tanh()
-        ################################################################################
-        #                              END OF YOUR CODE                                #
-        ################################################################################
-        self.init_hidden()
+        self.lstm = nn.LSTM(self.input_size, self.hidden_size, self.num_layers, batch_first=True)
+        self.linear_layer = nn.Linear(self.hidden_size, self.output_size)
+    
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).requires_grad_()
+        out, _ = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.linear_layer(out[:, -1, :]) 
+        return out
+    
+    def fit(self, x, y, epochs=100, lr=0.05, VERBOSE=False):
+        criterion = torch.nn.MSELoss(reduction = 'mean')
+        optimiser = torch.optim.Adam(self.parameters(), lr = lr)
+        if VERBOSE:
+            print(self.forward(x[:, :, :1]))
+            print(y)
+        hist = np.zeros(epochs)
+        for t in range(epochs):
+            pred = self.forward(x[:, :, :1])
+            loss = criterion(pred, y)
+            print("Epoch ", t, "Loss: ", loss.item())
+            hist[t] = loss.item()
+            optimiser.zero_grad()
+            loss.backward()
+            optimiser.step()
 
-    def init_hidden(self):
-        for p in self.parameters():
-            if p.data.ndimension() >= 2:
-                nn.init.xavier_uniform_(p.data)
-            else:
-                nn.init.zeros_(p.data)
 
-    def forward(self, x: torch.Tensor):
-        """Assumes x is of shape (batch, sequence, feature)"""
+# class LSTM(nn.Module):
 
-        ################################################################################
-        # TODO:                                                                        #
-        #   Implement the forward pass of LSTM. Please refer to the equations in the   #
-        #   corresponding section of jupyter notebook. Iterate through all the time    #
-        #   steps and return only the hidden and cell state, h_t and c_t.              #
-        #   h_t and c_t should be initialized to zeros.                                #
-        #   Note that this time you are also iterating over all of the time steps.     #
-        ################################################################################
-        h_t, c_t = torch.zeros(x.shape[0], self.hidden_size), torch.zeros(x.shape[0], self.hidden_size)
+#     def __init__(self, input_size=16, hidden_size=5, num_layers=4, output_size=1, dropout=0.2):
+#         super(LSTM, self).__init__()
+#         self.input_size = input_size
+#         self.hidden_size = hidden_size
+#         self.num_layers = num_layers
+#         self.output_size = output_size
+#         self.dropout = dropout
 
-        for time in range(x.shape[1]):
-            x_time = x[:, time, :]
-            # forget gate
-            f_t = self.sigmoid_ft(x_time @ self.W_if + self.b_if + h_t @ self.W_hf + self.b_hf)
-            # input gate
-            i_ts = self.sigmoid_it(x_time @ self.W_ii + self.b_ii + h_t @ self.W_hi + self.b_hi)
-            i_tt = self.tanh_gt(x_time @ self.W_ig + self.b_ig + h_t @ self.W_hg + self.b_hg)
-            # cell state
-            c_t = f_t * c_t + i_ts * i_tt
-            # output gate
-            o_t = self.sigmoid_it(x_time @ self.W_io + self.b_io + h_t @ self.W_ho + self.b_ho)
-            # hidden state
-            h_t = self.tanh_ot(c_t) * o_t
-        ################################################################################
-        #                              END OF YOUR CODE                                #
-        ################################################################################
-        return (h_t, c_t)
+#         self.encoder = nn.Linear(self.input_size, self.hidden_size)
+#         self.lstm = nn.LSTM(input_size=self.hidden_size, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True, dropout=self.dropout)
+#         self.decoder = nn.Linear(self.hidden_size, self.output_size)
+#         self.relu = nn.ReLU()
+#         self.init_weights()
+
+#     def init_weights(self):
+#         for name, param in self.lstm.named_parameters():
+#             if 'bias' in name:
+#                  nn.init.constant_(param, 0.0)
+#             elif 'weight_ih' in name:
+#                  nn.init.kaiming_normal_(param)
+#             elif 'weight_hh' in name:
+#                  nn.init.orthogonal_(param)
+
+#     def forward(self, x):
+#         out = self.encoder(x)
+#         out = self.relu(out)
+#         out, _ = self.lstm(out)
+#         # out = out[:, -1, :]
+#         out = out.squeeze(-1)
+#         return self.decoder(out)
+
+#     def fit(self, x, val=None, epochs=10, batch_size=60, lr=1e-4, VERBOSE=False):
+#         """Simple training script for """
+#         optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+#         dataset = TensorDataset(x)
+#         dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+#         for e in range(epochs):
+#             total_loss = 0
+#             for batch in dataloader:
+#                 inputs = batch[0]
+#                 loss_fxn = nn.MSELoss()
+#                 pred = self.forward(inputs[:, :, :-1])
+#                 print(pred.shape)
+#                 print(inputs[:, :, -1].shape)
+#                 loss = loss_fxn(pred, inputs[:, :, -1])
+#                 optimizer.zero_grad()
+#                 loss.backward()
+#                 optimizer.step()
+#                 total_loss += loss.item()
+#             if val is not None:
+#                 val_loss = nn.functional.mse_loss(self.forward(val), val)
+#                 if VERBOSE:
+#                     print("Epoch", str(e), "Loss:", total_loss, "Val Loss:", val_loss)
+#             elif VERBOSE:
+#                 print("Epoch", str(e), "Loss:", total_loss)
